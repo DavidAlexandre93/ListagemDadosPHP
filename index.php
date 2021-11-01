95d9f6d2c34a1542bdfb68871f7136eed4b7a7d2
David Alexandre Fernandes <davidalexandrefernandes@outlook.com>
2021-10-31 22:59:46 -0300

    Atualizacao de estilo

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <html lang="pt-br">
 	<head>
 		<meta charset="utf-8">
-		<title>Base de Dados</title>
+		<title>Base de dados</title>
 		<link rel="stylesheet" href="https://cdn.datatables.net/1.10.19/css/jquery.dataTables.min.css">
 		<script src="https://code.jquery.com/jquery-3.3.1.js"></script>
 		<script src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
@@ -45,8 +45,17 @@
 		</script>
 	</head>
 	<body>
+		<style>
+			thead{
+                color: blue;
+				font-weight: bold;
+            }
+			body{
+				background-color: #B0E0E6;
+			}
+			</style>
 
-		<h1>Listar dados</h1>
+		<h1 style="text-align:center">Dados de dividendos</h1>
 		<table id="listar-basea" class="display" style="width:100%">
 			<thead>
 				<tr>
